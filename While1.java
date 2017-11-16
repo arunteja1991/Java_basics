3101bfc37838fa6fcb2172e45f83f45ee5730171
arun teja g <arun.pinttu.teja@gmail.com>
2017-11-15 17:20:21 -0800

    all changes

--- While1.java
+++ While1.java
@@ -5,7 +5,7 @@ public class While1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int i = 0;
+		
 		while(i < 30){
 			i = i + 5;
 			System.out.println("this :" +i);
